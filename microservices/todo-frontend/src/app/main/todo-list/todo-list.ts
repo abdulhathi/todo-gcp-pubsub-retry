b881d98bd2c743eeb607c1395f7da599146a7b6c
Abdul Hathi <abdulhathi@gmail.com>
2026-03-24 21:48:39 -0700

    Save All todo, add todo and delete todo

--- microservices/todo-frontend/src/app/main/todo-list/todo-list.ts
+++ microservices/todo-frontend/src/app/main/todo-list/todo-list.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { TodoResponse, TodoService } from '../../services/todos.service';
 import { Todo } from '../../models/todo';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatIconModule, MatButtonModule],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.scss',
 })
 export class TodoList implements OnInit {
   todos: Todo[] = [];
+  newTodoTitle: string = '';
+  showAddTodo: boolean = false;
 
-  constructor(private todoService: TodoService) {}
+  constructor(
+    private todoService: TodoService,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
     this.todoService.getTodos().subscribe({
@@ -33,10 +40,30 @@ export class TodoList implements OnInit {
     todo.edited = false;
   }
 
+  addTodo() {
+    const todoIds = this.todos.map((t) => parseInt(t.id));
+    const id = Math.max(...todoIds) + 1;
+    this.todos.push({
+      id: String(id),
+      title: this.newTodoTitle,
+      status: 'PENDING',
+    });
+  }
+
+  deleteTodo(id: string) {
+    console.log(id);
+    this.todos = this.todos.filter((t) => t.id !== id);
+    this.cdr.detectChanges();
+  }
+
   onSave() {
-    // this.todoService.saveTodos(this.todos).subscribe(() => {
-    //   this.todos.forEach((t) => (t.status = 'SAVED'));
-    // });
+    this.todoService.saveTodos(this.todos).subscribe({
+      next: (data) => {
+        this.todos.forEach((t) => (t.status = 'SAVED'));
+        this.cdr.detectChanges();
+      },
+      error: (err: any) => console.log(err),
+    });
   }
 
   onSubmit() {
--- microservices/todo-frontend/src/app/services/todos.service.ts
+++ microservices/todo-frontend/src/app/services/todos.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
 
 export interface TodoResponse {
-  status: string
-  todos: Todo[]
+  status: string;
+  todos: Todo[];
 }
 
 @Injectable({
@@ -17,5 +17,9 @@ export class TodoService {
 
   getTodos() {
     return this.http.get<TodoResponse>(`${this.baseUrl}/api/todos`);
+  }
+
+  saveTodos(todos: Todo[]) {
+    return this.http.post(`${this.baseUrl}/api/todos/save`, { todos: todos });
   }
 }
